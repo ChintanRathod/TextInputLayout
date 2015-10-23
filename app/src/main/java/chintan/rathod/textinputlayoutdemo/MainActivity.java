e85cc0bce7d75ce48a67707661f730225b4082bc
rathodchintan <rathod.chintan@yahoo.com>
2015-10-24 01:38:22 +0530

    Part 2 First Commit

--- app/src/main/java/chintan/rathod/textinputlayoutdemo/MainActivity.java
+++ app/src/main/java/chintan/rathod/textinputlayoutdemo/MainActivity.java
@@ -1,8 +1,12 @@
 package chintan.rathod.textinputlayoutdemo;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Typeface;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -14,7 +18,7 @@ public class MainActivity extends ActionBarActivity {
 
     EditText firstName;
 
-    TextInputLayout tilFiratName,tilLastName;
+    TextInputLayout tilFiratName,tilLastName,tilEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,35 @@ public class MainActivity extends ActionBarActivity {
         firstName= (EditText) findViewById(R.id.edtFirstName);
         tilFiratName = (TextInputLayout) findViewById(R.id.tilFirstName);
         tilLastName = (TextInputLayout) findViewById(R.id.tilLastName);
+        tilEmail = (TextInputLayout)findViewById(R.id.tilEmail);
 
+        setTypefaceToInputLayout(tilFiratName,"KGCamdenMarketScript.ttf");
+
+        setTypefaceToInputLayout(tilLastName,"Adidas.ttf");
+
+        setTypefaceToInputLayout(tilEmail,"Organo.ttf");
+    }
+
+    private void setTypefaceToInputLayout(TextInputLayout inputLayout, String typeFace){
+
+        final Typeface tf = Typeface.createFromAsset(getAssets(), typeFace);
+
+        inputLayout.getEditText().setTypeface(tf);
+        try {
+            // Retrieve the CollapsingTextHelper Field
+            final Field collapsingTextHelperField = inputLayout.getClass().getDeclaredField("mCollapsingTextHelper");
+            collapsingTextHelperField.setAccessible(true);
+
+            // Retrieve an instance of CollapsingTextHelper and its TextPaint
+            final Object collapsingTextHelper = collapsingTextHelperField.get(inputLayout);
+            final Field tpf = collapsingTextHelper.getClass().getDeclaredField("mTextPaint");
+            tpf.setAccessible(true);
+
+            // Apply your Typeface to the CollapsingTextHelper TextPaint
+            ((TextPaint) tpf.get(collapsingTextHelper)).setTypeface(tf);
+        } catch (Exception ignored) {
+            // Nothing to do
+        }
     }
 
     @Override
