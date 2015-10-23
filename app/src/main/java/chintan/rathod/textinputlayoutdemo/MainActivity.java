38349e7ea86145050723fefcddd5f82bf5cbaa7b
rathodchintan <rathod.chintan@yahoo.com>
2015-10-24 00:46:59 +0530

    initial commit

--- app/src/main/java/chintan/rathod/textinputlayoutdemo/MainActivity.java
+++ app/src/main/java/chintan/rathod/textinputlayoutdemo/MainActivity.java
@@ -1,12 +1,8 @@
 package chintan.rathod.textinputlayoutdemo;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.Typeface;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.TextPaint;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -29,31 +25,6 @@ public class MainActivity extends ActionBarActivity {
         tilFiratName = (TextInputLayout) findViewById(R.id.tilFirstName);
         tilLastName = (TextInputLayout) findViewById(R.id.tilLastName);
 
-        //setTypefaceToInputLayout(tilFiratName,"KGCamdenMarketScript.ttf");
-
-        //setTypefaceToInputLayout(tilLastName,"Sansation_Regular.ttf");
-    }
-
-    private void setTypefaceToInputLayout(TextInputLayout inputLayout, String typeFace){
-
-        final Typeface tf = Typeface.createFromAsset(getAssets(), typeFace);
-
-        inputLayout.getEditText().setTypeface(tf);
-        try {
-            // Retrieve the CollapsingTextHelper Field
-            final Field collapsingTextHelperField = inputLayout.getClass().getDeclaredField("mCollapsingTextHelper");
-            collapsingTextHelperField.setAccessible(true);
-
-            // Retrieve an instance of CollapsingTextHelper and its TextPaint
-            final Object collapsingTextHelper = collapsingTextHelperField.get(inputLayout);
-            final Field tpf = collapsingTextHelper.getClass().getDeclaredField("mTextPaint");
-            tpf.setAccessible(true);
-
-            // Apply your Typeface to the CollapsingTextHelper TextPaint
-            ((TextPaint) tpf.get(collapsingTextHelper)).setTypeface(tf);
-        } catch (Exception ignored) {
-            // Nothing to do
-        }
     }
 
     @Override
